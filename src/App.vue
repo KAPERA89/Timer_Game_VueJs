e9dd439b3d8663c3fa2019a546a7e5f8088c8948
KAPERA89 <othmanedarhoni0@gmail.com>
2024-03-08 13:35:59 +0100

    game

---
+++ src/App.vue
@@ -0,0 +1,71 @@
+<template>
+  <h1>Othmane Reaction Timer</h1>
+  <button @click="start" :disabled="isPlaying">Play</button>
+  <Block :delay="delay"  v-if="isPlaying" @end="endGame"/>
+  <Results :score="score" v-if="this.showingResult"/>
+
+</template>
+
+
+
+<script>
+import Block from './components/Block.vue';
+import Results from './components/Results.vue'
+
+
+export default {
+  name: 'App',
+  components: {
+    Block,
+    Results,
+  },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+      score: null,
+      showingResult : false
+    }
+  },
+  methods: {
+    start(){
+      this.isPlaying = true
+      this.delay = 2000 + Math.random()*5000
+      //console.log(this.delay)
+    },
+    endGame(reactionTime){
+      this.score = reactionTime;
+      this.isPlaying = false;
+      this.showingResult = true
+    }
+  },
+}
+</script>
+
+
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #444;
+  margin-top: 60px;
+}
+button {
+  background: #0faf87;
+  color: white;
+  border: none;
+  padding: 8px 16px;
+  border-radius: 4px;
+  font-size: 16px;
+  letter-spacing: 1px;
+  cursor: pointer;
+  margin: 10px;
+}
+button[disabled]{
+  opacity: 0.2;
+  cursor: not-allowed;
+}
+</style>
